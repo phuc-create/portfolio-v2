0241f479fc16faa2cba365229602bf31e35ad4ae
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2023-03-22 17:35:57 +0700

    Added line and edges for geomatry

--- src/Scene/TorusKnotGeomatry.tsx
+++ src/Scene/TorusKnotGeomatry.tsx
@@ -1,13 +1,13 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
-import { Mesh, MeshBasicMaterial, TorusKnotGeometry } from 'three'
+import { BufferGeometry, Mesh, MeshBasicMaterial, TorusKnotGeometry } from 'three'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 
 const TorusKnotGeomatry = () => {
   // const geometry = new TorusKnotGeometry(10, 3, 100, 16)
   // const material = new MeshBasicMaterial({ color: 0xffff00 })
   // const torusKnot = new Mesh(geometry, material)
-  const viewport = useThree(state => state.viewport)
+  const geomatryRef = useRef<any>()
   return (
     <>
       <OrbitControls target={[0, 0, 0]} />
@@ -16,11 +16,13 @@ const TorusKnotGeomatry = () => {
       <Suspense fallback={null}>
         <mesh scale={[1, 1, 1]}>
           <torusKnotGeometry
+            ref={geomatryRef}
             args={[9.7, 3.5, 139, 14, 20, 3]}
-          // parameters={{ radius: 9.7, tube: 3.5, tubularSegments: 140, radialSegments: 19, p: 20, q: 3 }}
           />
           <meshStandardMaterial />
-        </mesh >
+          {/* <edgesGeometry attach="geometry" args={[geomatryRef.current]} />
+          <lineBasicMaterial attach="material" /> */}
+        </mesh>
       </Suspense>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
