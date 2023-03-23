22913ab2f5dd5a2d3f74c6838a99069254f15a45
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2023-03-23 17:03:15 +0700

    add dragon gltf

--- src/Scene/TorusKnotGeomatry.tsx
+++ src/Scene/TorusKnotGeomatry.tsx
@@ -2,7 +2,8 @@ import React, { Suspense, useMemo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { Mesh, TorusKnotGeometry } from 'three'
 import Camera from './Camera'
-import { Environment } from '@react-three/drei'
+import { Environment, useGLTF } from '@react-three/drei'
+import DragonModal from '../Modals/DragonModal'
 
 const TorusKnotGeomatry = () => {
   const torusRef = useRef<Mesh>(null!)
@@ -20,16 +21,18 @@ const TorusKnotGeomatry = () => {
 
   const torusGeomatry = useMemo(() => geomatryRef.current, [])
   const torusGeomatry2 = useMemo(() => geomatryRef2.current, [])
+
   return (
     <>
       <Camera position={[0, 0, 2]} />
-      {/* <color args={[0, 0, 0]} attach="background" /> */}
+      <color args={[0, 0, 0]} attach="background" />
       <ambientLight intensity={0.5} />
       <pointLight intensity={0.2} position={[-30, 10, 950]} />
       <pointLight intensity={0.2} position={[0, -10, 950]} />
       <pointLight intensity={0.2} position={[-30, 0, 950]} />
       <directionalLight position={[2, 0, 5]} shadow-mapSize={1024} castShadow />
       <group position={[0, 0, 0]}>
+        <DragonModal />
         <mesh scale={[0.1, 0.1, 0.1]} ref={torusRef} position={[2, 0, 0]}>
           <torusKnotGeometry ref={geomatryRef} args={[10, 3, 64, 8, 9, 17]} />
           <meshPhongMaterial color="#55efc4" />
