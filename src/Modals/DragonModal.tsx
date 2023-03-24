ff94ac1c92a14e3358a7ec4891ee11743d1f26b6
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2023-03-24 11:12:41 +0700

    Update source & added more modals

--- src/Modals/DragonModal.tsx
+++ src/Modals/DragonModal.tsx
@@ -6,12 +6,12 @@ import { Mesh } from 'three'
 
 const DragonModal = () => {
 
-  const gltf = useLoader(GLTFLoader, "../gltf/scene.gltf")
+  const gltf = useLoader(GLTFLoader, "./gltf/cyberpunk_dragon_ld34/scene.gltf")
 
   useEffect(() => {
-    gltf.scene.scale.set(1, 1, 1)
-    gltf.scene.rotation.x = 0
-    gltf.scene.rotation.y = 3.3
+    gltf.scene.scale.set(2, 2, 2)
+    // gltf.scene.rotation.x = 0
+    // gltf.scene.rotation.y = 3.3
     gltf.scene.traverse((object) => {
       if (object instanceof Mesh) {
         object.castShadow = true
@@ -22,7 +22,7 @@ const DragonModal = () => {
   }, [gltf])
 
   return (
-    <primitive object={gltf.scene} position={[0, -0.5, 2]} />
+    <primitive object={gltf.scene} position={[0, -2, 2]} />
   )
 }
 
